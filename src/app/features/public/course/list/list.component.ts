9fcf415fdaa2be196db39425f2706581c98907cc
Fahd Elgayed <elgayed.fahd@esprit.tn>
2025-02-19 14:56:38 +0100

    Display fetched courses

--- src/app/features/public/course/list/list.component.ts
+++ src/app/features/public/course/list/list.component.ts
@@ -1,22 +1,25 @@
 import {Component, OnInit} from '@angular/core';
-import {CourseService} from "../../../../services/course-managment/course.service";
+import {Course, CourseService} from "../../../../services/course-managment/course.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'course-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  courses: any;
+  courses: Course[] = [];
   constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courseService.fetchAllCourses().subscribe( // TODO subscribe is deprecated
-        (data) => {this.courses = data; console.log(this.courses)},
-        (error) => console.error('Error fetching courses list:', error)
-    );
+    this.courseService.fetchAllCourses().subscribe({// TODO subscribe is deprecated
+        next: data => this.courses = data,
+        error: error => console.error('Error fetching courses list:', error)
+    });
   }
 
 }
