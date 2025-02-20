a613577ad95b658b35e771f4b370ab8a2778e2dc
aziz913 <aziz.bakloti@sprit.tn>
2025-02-20 12:31:53 +0100

    latest

--- src/app/features/Stage/edit-stage/edit-stage.component.ts
+++ src/app/features/Stage/edit-stage/edit-stage.component.ts
@@ -4,25 +4,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Stage } from 'src/app/models/Stage';
 import { StageService } from 'src/app/services/stage.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-edit-stage',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './edit-stage.component.html',
-  styleUrls: ['./edit-stage.component.css']
+  styleUrls: ['./edit-stage.component.css'],
+  providers: [DatePipe]
 })
 export class EditStageComponent {
 
   stageForm: FormGroup;
-  
   stageId!: number;
-  
   stage: Stage;
 
-  constructor(private fb: FormBuilder,private serv: StageService,private router: Router,private route: ActivatedRoute) { 
+  constructor(
+    private fb: FormBuilder,
+    private serv: StageService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private datePipe: DatePipe
+  ) { 
 
-    // hnee controle saisie
     this.stageForm = this.fb.group({
       title: ['', Validators.required],
       description: ['', Validators.required],
@@ -37,26 +42,27 @@ export class EditStageComponent {
       startDate: new Date(),
       endDate: new Date()
     }
-
-
   }
 
   ngOnInit() {
     this.stageId = Number(this.route.snapshot.paramMap.get('id'));
     if (this.stageId) {
       this.serv.getStageById(this.stageId).subscribe((stage) => {
-        this.stageForm.patchValue(stage);
         this.stage = stage;
+        this.stageForm.patchValue({
+          title: stage.title,
+          description: stage.description,
+          startDate: this.datePipe.transform(stage.startDate, 'yyyy-MM-dd'),
+          endDate: this.datePipe.transform(stage.endDate, 'yyyy-MM-dd')
+        });
       });
     }
   }
 
   onSubmit() {
     if (this.stageForm.valid) {
-      console.log(this.stageForm.value);
-      console.log(this.stageId);
-
-      this.serv.updateStage(this.stageId, this.stage).subscribe(
+      const updatedStage = { ...this.stage, ...this.stageForm.value };
+      this.serv.updateStage(this.stageId, updatedStage).subscribe(
         (data) => {
           console.log(data);
           this.router.navigate(['/stages']);
@@ -64,7 +70,6 @@ export class EditStageComponent {
       );
     }
   }
-
 
   deleteStage() {
     this.serv.deleteStage(this.stageId).subscribe(
