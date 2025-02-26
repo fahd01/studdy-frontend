0a86692c0f44dacbd0b2e4cdf721113dd8c77ec9
Fahd Elgayed <elgayed.fahd@esprit.tn>
2025-02-26 10:39:36 +0100

    Fix nav bar transparence / white color when scrolling

--- src/app/layouts/landing-layout/landing-navbar/landing-navbar.component.ts
+++ src/app/layouts/landing-layout/landing-navbar/landing-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,6 +6,35 @@ import { Router } from '@angular/router';
   templateUrl: './landing-navbar.component.html',
   styleUrls: ['./landing-navbar.component.css']
 })
-export class LandingNavbarComponent {
+export class LandingNavbarComponent implements OnInit{
+
+  private lastScrollTop = 0;
+  private navbar: HTMLElement | null = null;
+
+  ngOnInit(): void {
+    this.navbar = document.getElementById('ftco-navbar');
+  }
+
+  //@HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (!this.navbar) return;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > this.lastScrollTop) {
+      // Scroll down
+      this.navbar.classList.add('navbar-scrolled-down');
+      this.navbar.classList.remove('navbar-scrolled-up');
+    } else {
+      // Scroll up
+      if (scrollTop > 0) {
+        this.navbar.classList.add('navbar-scrolled-up');
+        this.navbar.classList.remove('navbar-scrolled-down');
+      } else {
+        this.navbar.classList.remove('navbar-scrolled-up', 'navbar-scrolled-down');
+      }
+    }
+
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+  }
 
 }
