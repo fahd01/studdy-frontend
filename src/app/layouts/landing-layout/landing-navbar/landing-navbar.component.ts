7480a307b506f3cd2251ae23942e8ae82a5858c7
Fahd Elgayed <elgayed.fahd@esprit.tn>
2025-02-26 10:39:36 +0100

    Fix nav bar transparence / white color when scrolling

--- src/app/layouts/landing-layout/landing-navbar/landing-navbar.component.ts
+++ src/app/layouts/landing-layout/landing-navbar/landing-navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-landing-navbar',
@@ -7,5 +6,4 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-navbar.component.css']
 })
 export class LandingNavbarComponent {
-
 }
