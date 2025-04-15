fb894d480f97c3945650ed16a440f2f16cd7d665
Fahd Elgayed <elgayed.fahd@esprit.tn>
2025-04-16 00:01:26 +0100

    Merge app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
 import { TranslationService } from './services/Translation/translation.service';
 
@@ -8,11 +8,40 @@ import { TranslationService } from './services/Translation/translation.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Studdy Learning Platform';
+  private lastScrollTop = 0;
+
   isLoginPage: boolean = false;
   isRegisterPage: boolean = false;
   isPasswordResetPage: boolean = false;
 
-  constructor(private router: Router,private translationService: TranslationService) {}
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+    private translationService: TranslationService
+  ) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    const navbar = document.getElementById('ftco-navbar');
+
+    if (st > this.lastScrollTop) {
+      // Scroll Down
+      this.renderer.removeClass(navbar, 'navbar-scrolled-up');
+      this.renderer.addClass(navbar, 'navbar-scrolled-down');
+    } else {
+      // Scroll Up
+      this.renderer.removeClass(navbar, 'navbar-scrolled-down');
+      this.renderer.addClass(navbar, 'navbar-scrolled-up');
+    }
+
+    this.lastScrollTop = Math.max(st, 0);
+  }
+
+  isDashboardRoute(): boolean {
+    return this.router.url.startsWith('/dashboard');
+  }
 
   ngOnInit(): void {
     this.router.events.subscribe((event) => {
@@ -30,9 +59,9 @@ export class AppComponent {
     });
 
 
-    const savedLang = localStorage.getItem('userLang') || 
-    navigator.language.split('-')[0] || 
+    const savedLang = localStorage.getItem('userLang') ||
+    navigator.language.split('-')[0] ||
     'fr';
-this.translationService.translateApp(savedLang);
+    this.translationService.translateApp(savedLang);
   }
 }
